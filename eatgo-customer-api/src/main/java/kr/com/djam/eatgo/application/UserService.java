c86941c1f1969247b7c85ce152628c1d6d7fc292
오주담 <dhwnkea@naver.com>
2020-12-23 16:30:02 +0900

    패스워드 인코딩 적용

--- eatgo-customer-api/src/main/java/kr/com/djam/eatgo/application/UserService.java
+++ eatgo-customer-api/src/main/java/kr/com/djam/eatgo/application/UserService.java
@@ -3,9 +3,12 @@ package kr.com.djam.eatgo.application;
 import kr.com.djam.eatgo.domain.User;
 import kr.com.djam.eatgo.domain.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.Optional;
 
 @Service
 @Transactional
@@ -19,12 +22,19 @@ public class UserService {
     }
 
     public User registerUser(String email, String name, String password){
-        System.out.println("service");
+        Optional<User> existed = userRepository.findByEmail(email);
+        if(existed.isPresent()){
+            throw new EmailExistedException(email);
+        }
+        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        String encodedPassword = passwordEncoder.encode(password);
+
         User user = User.builder()
                 .id(1004L)
                 .email(email)
                 .name(name)
-                .password(password)
+                .password(encodedPassword)
+                .level(1L)
                 .build();
 
         System.out.println(user.getId() + " : " + user.getEmail());
--- eatgo-customer-api/src/test/java/kr/com/djam/eatgo/application/UserServiceTests.java
+++ eatgo-customer-api/src/test/java/kr/com/djam/eatgo/application/UserServiceTests.java
@@ -1,17 +1,22 @@
 package kr.com.djam.eatgo.application;
 
+import kr.com.djam.eatgo.domain.User;
 import kr.com.djam.eatgo.domain.UserRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import java.util.Optional;
+
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 
 class UserServiceTests {
 
-    private UserService userSevice;
+    private UserService userService;
 
     @Mock
     private UserRepository userRepository;
@@ -19,7 +24,7 @@ class UserServiceTests {
     @BeforeEach
     public void setUp(){
         MockitoAnnotations.initMocks(this);
-        userSevice = new UserService(userRepository);
+        userService = new UserService(userRepository);
     }
     @Test
     public void registerUser(){
@@ -27,9 +32,24 @@ class UserServiceTests {
         String name = "Tester";
         String password = "test";
 
-        userSevice.registerUser(email, name, password);
+        userService.registerUser(email, name, password);
 
         verify(userRepository).save(any());
     }
+
+    @org.junit.Test(expected = EmailExistedException.class)
+    public void registerUserWithExistedEmail(){
+
+        String email = "tester@example.com";
+        String name = "Tester";
+        String password = "test";
+
+        User mockUser = User.builder().build();
+        given(userRepository.findByEmail(email)).willReturn(Optional.of(mockUser));
+        userService.registerUser(email, name, password);
+
+        verify(userRepository, never()).save(any());
+    }
+
 
 }
